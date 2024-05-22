0829eef59a4a04d008f57d5212ed9c537a26a639
ayushi-sahu <111413228+12-ayushi@users.noreply.github.com>
2024-05-22 09:52:34 +0530

    Update LinkedHashSetDemo.java

--- LinkedHashSetDemo.java
+++ LinkedHashSetDemo.java
@@ -5,6 +5,11 @@ import java.util.LinkedHashSet;
 public class LinkedHashSetDemo {
     public static void main(String[] args) {
         LinkedHashSet s=new LinkedHashSet();
-
+        s.add(10);
+        s.add(20);
+        s.add(30);
+        System.out.println(s);
+        System.out.println(s.size());
+        
     }
 }
